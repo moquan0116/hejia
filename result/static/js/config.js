96aa7b0da7ccc698e32bda2eab40e074db1eacc6
moquan0116 <330898472@qq.com>
2017-12-09 21:46:37 +0800

    图片重下

--- result/static/js/config.js
+++ result/static/js/config.js
@@ -2,14 +2,14 @@
  * Created by Administrator on 2017/12/9.
  */
 var subscribe = {
-    'project':'my_host/hejia/index.php',
+    'project':'hejia/index.php',
     'method':'main/addPhone'
 };
 var goods = {
-    'project':'my_host/hejia/index.php',
+    'project':'hejia/index.php',
     'method':'main/getAddGoods'
 };
 var brand = {
-    'project':'my_host/hejia/index.php',
+    'project':'hejia/index.php',
     'method':'main/getAddGoods'
 };
